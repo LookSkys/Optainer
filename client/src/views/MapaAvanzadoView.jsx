3f436f743e2387fdc057f18c716922718aab1fda
Mojedas <matias.ojeda.saa@gmail.com>
2024-10-17 13:01:14 -0300

    botones profundidad mejora UX

--- client/src/views/MapaAvanzadoView.jsx
+++ client/src/views/MapaAvanzadoView.jsx
@@ -198,17 +198,17 @@ function MapaAvanzadoView() {
                 </div>
                 <div className="col-1" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                     <div>
-                    {[1, 2, 3].map((nivel) => (
-                        <div className="row">
-                        <button type="button" className="btn btn-danger boton-profundidad "
-                            key={nivel}
-                            onClick={() => cambiarProfundidad(nivel)}
-                            disabled={profundidadActual === nivel}
-                        >
-                            {nivel}
-                        </button>
-                        </div>
-                    ))}
+                        {[1, 2, 3].map((nivel) => (
+                            <div className="row" key={nivel}>
+                                <button 
+                                    type="button" 
+                                    className={`btn boton-profundidad ${profundidadActual === nivel ? 'boton-seleccionado' : 'boton-no-seleccionado'}`}
+                                    onClick={() => cambiarProfundidad(nivel)}
+                                >
+                                    {nivel}
+                                </button>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
