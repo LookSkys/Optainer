c927e1dc2b1d23d63e5245c0e2bac973543e4dc0
Enrique Sanhueza <sanhue.kike@gmail.com>
2024-11-06 22:52:11 -0300

    tt

--- client/src/views/LoginView.jsx
+++ client/src/views/LoginView.jsx
@@ -1,4 +1,4 @@
-import Login from '../components/Login/Login'; // Importa el componente Login
+import{ Login }from '../components/Login/Login'; // Importa el componente Login
 
 function LoginView() {
   return (
