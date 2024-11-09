323162d1035576b13f1ebe241051a23001e3dd26
Enrique Sanhueza <sanhue.kike@gmail.com>
2024-11-09 17:32:55 -0300

    ....

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.DB_CONNECTION)
   .catch(console.log);
 
 // const corsOptions = {
-//     origin: ['https://optainerfront.vercel.app', 'http://192.168.100.11:5173'],
+//     origin: ['https://optainerfront.vercel.app'],
 //     optionsSuccessStatus: 200,
 // };
 
