b48b64438361524c289816c6a230c860f9f9ae87
RGTEMISF <raul.cabrera@sansano.usm.cl>
2024-11-14 12:22:55 -0300

    Codigo de mapa avanzado optimizado/modularizado en componentes y creación de componente de hora actual

---
+++ client/src/components/NumerosLateralDerecha/NumerosLateralDerecha.jsx
@@ -0,0 +1,11 @@
+export function NumerosLateralDerecha () {
+    return(
+        <div className="col-1">
+            <br /><h2>5</h2> <br /><br /><br />
+            <h2>4</h2> <br /><br /><br />
+            <h2>3</h2> <br /><br /><br />
+            <h2>2</h2> <br /> <br /> <br />
+            <h2>1</h2> 
+        </div>
+    )
+}
